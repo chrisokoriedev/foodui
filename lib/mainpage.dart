a672e8526a9f950064ef3ca339cdbee2520fe89d
ChrisIuil <okoriec01@gmail.com>
2022-11-11 17:59:14 +0100

    hmm

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -7,49 +7,51 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: kDefaultPadding,
-          child: Center(
-            child: Column(
-              children: [
-                sizedHeight(50),
-                const Icon(
-                  FontAwesomeIcons.bowlRice,
-                  size: 80,
-                  color: kPrimaryColor,
-                ),
-                sizedHeight(20),
-                RichText(
-                  textAlign: TextAlign.center,
-                  text: const TextSpan(
-                    text: 'Welcome To',
-                    style: TextStyle(
-                        color: kBlackColor,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w600),
-                    children: [
-                      TextSpan(
-                        text: '\nTastic',
-                        style: TextStyle(
-                          color: kPrimaryColor,
-                        ),
-                      )
-                    ],
+    return Container(
+      decoration: BoxDecoration(color: kBlackColor),
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: kDefaultPadding,
+            child: Center(
+              child: Column(
+                children: [
+                  sizedHeight(50),
+                  const Icon(
+                    FontAwesomeIcons.bowlRice,
+                    size: 80,
+                    color: kPrimaryColor,
                   ),
-                ),
-                sizedHeight(20),
-                const Text(
-                  'Start browsing the library of 10,000+ \nhealthly recipes today',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: kGreyColor),
-                ),
-                
-              ],
+                  sizedHeight(20),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: const TextSpan(
+                      text: 'Welcome To',
+                      style: TextStyle(
+                          color: kBlackColor,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w600),
+                      children: [
+                        TextSpan(
+                          text: '\nTastic',
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  sizedHeight(20),
+                  const Text(
+                    'Start browsing the library of 10,000+ \nhealthly recipes today',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: kGreyColor),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
